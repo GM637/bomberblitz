3dea19dfd0f5d3a5c5b9077c16e9b044de49a2a0
Mathieu Joly <m.joly@live.fr>
2023-07-16 16:18:32 +0200

    separate both worlds & update position & rotation

--- src/3d/Players.jsx
+++ src/3d/Players.jsx
@@ -30,15 +30,13 @@ const LocalPlayer = forwardRef(({ position, rotation, animation }, ref) => {
       position={position}
     >
       <CapsuleCollider args={[0.12, 0.3]} />
-      <Suspense fallback={null}>
-        <Character animation={animation} />
-      </Suspense>
     </RigidBody>
   );
 });
 
 export default function Players() {
   const [players, setPlayers] = useState([]);
+  const [bodies, setBodies] = useState([]);
   const [characters, setCharacters] = useState([]);
 
   const up = useKeyboardControls((state) => state.up);
@@ -50,28 +48,36 @@ export default function Players() {
 
   useEffect(() => {
     onPlayerJoin(async (state) => {
-      const ref = createRef();
+      const playerRef = createRef();
+      const bodyRef = createRef();
 
-      const currCharacter = isHost() ? (
-        <LocalPlayer
-          key={state.id}
-          ref={ref}
-          position={[0, 3, 0]}
-          rotation={[0, 0, 0]}
-          animation={"Idle"}
-        />
-      ) : (
+      const currCharacter = (
         <SocketPlayer
           key={state.id}
-          ref={ref}
+          ref={playerRef}
           position={[0, 0, 0]}
           rotation={[0, 0, 0]}
           animation={"Idle"}
         />
       );
 
-      setPlayers((players) => [...players, { state, ref }]);
+      let currBody;
+
+      if (isHost()) {
+        currBody = (
+          <LocalPlayer
+            key={state.id}
+            ref={bodyRef}
+            position={[0, 3, 0]}
+            rotation={[0, 0, 0]}
+            animation={"Idle"}
+          />
+        );
+      }
+
+      setPlayers((players) => [...players, { state, playerRef, bodyRef }]);
       setCharacters((characters) => [...characters, currCharacter]);
+      if (isHost()) setBodies((bodies) => [...bodies, currBody]);
 
       state.onQuit(() => {
         setCharacters((characters) =>
@@ -89,39 +95,82 @@ export default function Players() {
     // update shared direction inputs
     myPlayer().setState("dir", direction);
 
-    if (isHost()) {
-      for (const player of players) {
-        const { ref, state } = player;
-        if (!ref.current) continue;
+    for (const player of players) {
+      const state = player.state;
 
-        const dir = state.getState("dir");
+      const dir = state.getState("dir");
+
+      // update collider position if is host
+      if (isHost()) {
+        const bodyRef = player.bodyRef;
+        if (!bodyRef?.current) continue;
 
         //move rigidbody
-        const impulse = { x: dir.x * MOVEMENT_SPEED * delta, y: 0, z: dir.z * MOVEMENT_SPEED * delta }
-        ref.current.applyImpulse(impulse, true);
-
-
-
+        const impulse = {
+          x: dir.x * MOVEMENT_SPEED * delta,
+          y: 0,
+          z: dir.z * MOVEMENT_SPEED * delta,
+        };
+        bodyRef.current.applyImpulse(impulse, true);
 
         // update shared position
-        const pos = ref.current.translation();
+        const pos = bodyRef.current.translation();
         state.setState("pos", pos);
-      }
-    } else {
-      for (const player of players) {
-        const { ref, state } = player;
-        if (!ref?.current) continue;
+
+        // update character
+        const playerRef = player.playerRef;
+        if (!playerRef?.current) continue;
+
+        // move the player
+        const playerPos = playerRef.current.position;
+        if (
+          playerPos.x !== pos.x ||
+          playerPos.y !== pos.y ||
+          playerPos.z !== pos.z
+        ) {
+          playerRef.current.position.x = pos.x;
+          playerRef.current.position.y = pos.y;
+          playerRef.current.position.z = pos.z;
+
+          // update shared position
+          state.setState("pos", pos);
+        }
+
+        // rotate the player
+        if (dir.x || dir.z) {
+          const angle = Math.atan2(dir.x, dir.z);
+          playerRef.current.rotation.y = angle;
+
+          // update shared rotation
+          const rot = playerRef.current.rotation;
+          state.setState("rot", rot);
+        }
+      } else {
+        // update character
+        const playerRef = player.playerRef;
+        if (!playerRef?.current) continue;
 
         // retrieve shared position
         const pos = state.getState("pos");
         if (!pos) continue;
 
-        ref.current.position.x = pos.x;
-        ref.current.position.y = pos.y;
-        ref.current.position.z = pos.z;
+        playerRef.current.position.x = pos.x;
+        playerRef.current.position.y = pos.y;
+        playerRef.current.position.z = pos.z;
+
+        // retrieve shared rotation
+        const rot = state.getState("rot");
+        if (!rot) continue;
+
+        playerRef.current.rotation.y = rot.y;
       }
     }
   });
 
-  return <group>{characters}</group>;
+  return (
+    <group>
+      {characters}
+      {bodies}
+    </group>
+  );
 }
