ac3100c381c3a7b8d3f1b36122fa324133afed6d
Mathieu Joly <m.joly@live.fr>
2023-07-18 01:18:09 +0200

    add smooth rotation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,15 @@
-export const VELOCITY = 2.5;
+export const VELOCITY = 4;
 export const FIRE_SPEED = 1;
 export const RUN_SPEED = 2;
-export const SONIC_SPEED = 4;
+export const SONIC_SPEED = 4.5;
 export const MAX_SPEED = 5;
+
+export const ROTATION_SPEED = 5;
 
 export const BOMB_SPEED = 0.5;
 export const BOMB_INTERVAL = 3;
 export const BOMB_DISTANCE = 1;
 
-export const ROT_SPEED = 2;
+export const ROT_SPEED = 5;
 
 export const AREA_SIZE = 3;
