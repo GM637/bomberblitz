35ee7c0959066f368c9c0f918ea489d3eb1bbf3d
Mathieu Joly <m.joly@live.fr>
2023-07-18 00:43:16 +0200

    add FireRun animation

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,8 +1,10 @@
-import { RUN_SPEED, SONIC_SPEED } from "./constants";
+import { RUN_SPEED, SONIC_SPEED, FIRE_SPEED } from "./constants";
 
 export function getAnimation(linvel) {
   const velocity = Math.abs(linvel.x) + Math.abs(linvel.z);
+
   if (velocity > SONIC_SPEED) return "SonicRun";
   if (velocity > RUN_SPEED) return "Run";
+  if (velocity > FIRE_SPEED) return "FireRun";
   return "Idle";
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
-export const VELOCITY = 2;
-export const RUN_SPEED = 1;
-export const SONIC_SPEED = 3;
+export const VELOCITY = 2.5;
+export const FIRE_SPEED = 1;
+export const RUN_SPEED = 2;
+export const SONIC_SPEED = 4;
 export const MAX_SPEED = 5;
 
 export const BOMB_SPEED = 0.5;
