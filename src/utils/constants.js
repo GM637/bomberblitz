ec462eebdf9836800b9c61f2b661a2f1f92642f4
Mathieu Joly <m.joly@live.fr>
2023-07-18 13:49:44 +0200

    add bomb scale change over time

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
+/*
+// // // CHARACTER
+*/
 export const VELOCITY = 4;
 export const FIRE_SPEED = 1;
 export const RUN_SPEED = 2;
@@ -6,10 +9,19 @@ export const MAX_SPEED = 5;
 
 export const ROTATION_SPEED = 5;
 
+/*
+// // // BOMB
+*/
 export const BOMB_SPEED = 0.5;
 export const BOMB_INTERVAL = 3;
 export const BOMB_DISTANCE = 1;
 
+export const BOMB_SCALE_FACTOR = 0.2;
+export const BOMB_SCALE_SPEED = 3;
+
 export const ROT_SPEED = 5;
 
+/*
+// // // ARENA
+*/
 export const AREA_SIZE = 3;
